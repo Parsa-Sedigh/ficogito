ebb015da4498951c2d09e6005b2ecc047a83fc4b
parsa <parsa.sedigh78@gmail.com>
2021-10-22 21:58:01 +0330

    favicon added and made responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
-import logo from '../public/images/FiCogito-logo.png';
+import logo from '../public/images/FiCogito-logo-hexagonal.jpg';
+import logoText from '../public/images/FICogito-text.jpg';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Image className={styles.logo} src={logo} width={500} height={200} alt="ficogito"/>
+      <Image className={styles.logo} src={logo} width={140} height={140} alt="ficogito"/>
+      <Image src={logoText} width={578} height={100} alt="ficogito"/>
     </div>
   )
-}
+};
 
 export default Home
