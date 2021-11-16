eef540dcabfbfc71b06d7e4f1f49790127c7b444
parsa <parsa.sedigh78@gmail.com>
2021-11-17 00:34:49 +0330

    A complete path drew

--- components/Paths/Paths.tsx
+++ components/Paths/Paths.tsx
@@ -14,7 +14,33 @@ export const Paths = () => {
                 alignItems: 'end'
             }}>
                 <div className={`${styles.path} ${styles['path--vertical-long']}`} style={{gridColumn: '1/2'}} />
-                <div className={`${styles.path} ${styles['path--vertical-short']}`} style={{gridColumn: '3/-1', justifySelf: 'end'}} />
+
+                <div style={{
+                    gridColumn: '2/-1',
+                    position: 'relative',
+                    height: '100%'
+                }}>
+                    <div className={`${styles.path} ${styles['path--vertical-short']}`} style={{
+                        gridColumn: '3/-1',
+                        position: 'absolute',
+                        height: '100%',
+                        right: '0'
+                    }} />
+                    <div className={`${styles.path}`} style={{
+                        position: 'absolute',
+                        bottom: '-31.9%',
+                        right: '26%',
+                        transform: 'rotate(90deg)',
+                        height: '65%'
+                    }} />
+                    <div className={`${styles.path}`} style={{
+                        position: 'absolute',
+                        bottom: '-19%',
+                        right: '51.5%',
+                        height: '20%'
+                    }} />
+                </div>
+
             </div>
 
             <div className={styles['main-image']}>
