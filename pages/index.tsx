a5825f1e9ea5f91eaa8b4a5f1166a7535b96fa4e
parsa <parsa.sedigh78@gmail.com>
2021-12-04 00:31:59 +0330

    styles of header changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ const particlesOptions = {
 
 const Home: NextPage = () => {
     return (
-        <>
+        <div className={styles['page-container']}>
             <div className={styles.particles}>
                 <div className={styles['background--right']} />
                 <Particles options={particlesOptions}   />
@@ -101,6 +101,19 @@ const Home: NextPage = () => {
                     </div>
 
                 </div>
+            </div>
+
+            <div className={styles['dots-container']}>
+                <svg className={`${styles['dots']}`}
+                     width="300" height="684" viewBox="0 0 404 784">
+                    <defs>
+                        <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20"
+                                 patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor"></rect>
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="784" fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)"></rect>
+                </svg>
             </div>
 
 
@@ -122,7 +135,7 @@ const Home: NextPage = () => {
             {/*</div>*/}
 
             {/*<Steps />*/}
-        </>
+        </div>
     )
 };
 
