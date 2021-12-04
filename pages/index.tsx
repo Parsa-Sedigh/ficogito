6147dc5953892758a302098ae7c2f65e586c97c8
Parsa_Sedigh <parsa.sedigh78@gmail.com>
2021-12-04 11:15:43 +0330

    dots changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ const Home: NextPage = () => {
 
             <div className={styles['dots-container']}>
                 <svg className={`${styles['dots']}`}
-                     width="300" height="684" viewBox="0 0 404 784">
+                     width="1200" height="860" viewBox="0 0 404 784">
                     <defs>
                         <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20"
                                  patternUnits="userSpaceOnUse">
