a5f3f445df8ce889edd44bd00b3152875d65f497
Parsa_Sedigh <parsa.sedigh78@gmail.com>
2021-12-04 10:18:35 +0330

    eslint rule disabled

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
