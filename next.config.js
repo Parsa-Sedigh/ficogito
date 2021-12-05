843129f97db4a0e73a8eb667f8b8bfe2a96699f3
parsa <parsa.sedigh78@gmail.com>
2021-12-05 23:27:05 +0330

    Working on make a dots svg background

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  typescript: {ignoreBuildErrors: true}
+  typescript: {ignoreBuildErrors: true} /* TODO: Remove this ASAP */
 }
